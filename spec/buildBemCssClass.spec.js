74359aedc2f81bcb9ab4927d52a6eefb6b304675
dfilatov <dfilatov@yandex-team.ru>
2014-11-25 11:06:12 +0300

    add specs

---
+++ spec/buildBemCssClass.spec.js
@@ -0,0 +1,41 @@
+var buildBemClassName = require('../lib/buildBemClassName');
+
+describe('buildBemClassName', function() {
+    it('should build block class name', function() {
+        expect(buildBemClassName('button'))
+            .toBe('button');
+    });
+
+    it('should build block with modifiers class name', function() {
+        expect(
+            buildBemClassName('button', { mod1 : 'val1', mod2 : 'val2' }))
+            .toBe('button button_mod1_val1 button_mod2_val2');
+    });
+
+    it('should build elem class name', function() {
+        expect(buildBemClassName('button', 'box'))
+            .toBe('button__box');
+    });
+
+    it('should build elem with modifiers class name', function() {
+        expect(buildBemClassName('button', 'box', { mod1 : 'val1', mod2 : 'val2' }))
+            .toBe('button__box button__box_mod1_val1 button__box_mod2_val2');
+    });
+
+    it('should build block with mixed elem class name', function() {
+        expect(buildBemClassName('button', null, [{ block : 'mixed', elem : 'elem' }]))
+            .toBe('button mixed__elem');
+    });
+
+    it('should throw error if mix doesn\'t contain only block', function() {
+        expect(function() {
+            buildBemClassName('button', null, [{ block : 'mixed' }])
+        }).toThrowError('Specify both block and elem in mix');
+    });
+
+    it('should throw error if mix doesn\'t contain only elem', function() {
+        expect(function() {
+            buildBemClassName('button', null, [{ elem : 'mixed' }])
+        }).toThrowError('Specify both block and elem in mix');
+    });
+});
