ed539bccf5421c1cf33fb529ce5c648ee87ecba2
NeZeD [Mac Air] <nezed@ya.ru>
2015-10-20 03:26:26 +0300

    Update example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,8 @@
 var bemReact = require('../lib/bemReact'),
+    React = require('react'),
+    ReactDOM = require('react-dom'),
     Dropdown = require('./components/dropdown');
 
-bemReact.render(
-    { block : Dropdown, props : { content : 'dropdown content' } },
+ReactDOM.render(
+    React.createElement( Dropdown, { content : 'dropdown content' } ),
     document.body);
